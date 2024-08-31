0c614ae4c688cce9633ba8da4d37be6250a27a5f
rayyan749 <hassanrayyan749@gmail.com>
2024-08-31 21:32:23 +0500

    work ai

--- computer/src/app/page.tsx
+++ computer/src/app/page.tsx
@@ -1,22 +1,26 @@
-import React from "react"
+import React from "react";
 
-const page = () => {
+const Page = () => {
   return (
     <div>
-      <figure className="md:flex bg-yellow-100 rounded-x5 p-20 md:p-300 dark:bg-slate-1000">
-        <img className="w-80 h-60 md:w-60 md:rounded-none rounded-full mx-auto" src="/reyyan.jpg" alt="" width="100" height="512" />
-        <div className="pt-6 md:p-40 text-center md:text-left space-y-2">
+      <figure className="md:flex bg-yellow-100 rounded-xl p-20 md:p-40 dark:bg-slate-1000">
+        <img
+          className="w-80 h-60 md:w-60 md:h-60 md:rounded-none rounded-full mx-auto"
+          src="/reyyan.jpg"
+          alt="Portrait of Reyyan Hassan"
+        />
+        <div className="pt-6 md:p-10 text-center md:text-left space-y-2">
           <blockquote>
-            <p className="text-lg font-large">
-              “ My Name is Reyyan. I recently completed my F.sc And I am joining Generative AI . MY teacher,s name is Sir Asif Langrah.,.”
+            <p className="text-lg font-medium">
+              “My name is Reyyan. I recently completed my F.Sc and am joining the field of Generative AI. My teacher’s name is Sir Asif Langrah.”
             </p>
           </blockquote>
           <figcaption className="font-medium">
             <div className="text-sky-500 dark:text-sky-400">
-              Rayyan Hassan
+              Reyyan Hassan
             </div>
             <div className="text-slate-1000 dark:text-slate-600">
-              Artificial Intelligence, (Student)
+              Artificial Intelligence (Student)
             </div>
           </figcaption>
         </div>
@@ -25,4 +29,4 @@ const page = () => {
   );
 };
 
-export default page
+export default Page;
